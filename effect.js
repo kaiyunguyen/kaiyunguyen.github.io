3d74e22a2715164809d8da050727b0bbe15648ec
aki <kaiyunguyen@gmail.com>
2026-01-29 12:25:02 -0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -3,10 +3,25 @@ let refreshTimer;
 // initial auto-refresh (15s)
 refreshTimer = setTimeout(() => {
     location.reload();
-}, 15000)
+}, 15000);
 
 const welcome = document.getElementById("welcomeScreen");
 const photobooth = document.getElementById("photoboothScreen");
+
+const webcamVideo = document.getElementById("webcamVideo");
+const constraints = {
+    video: {
+        width: 1280,
+        height: 720,
+        facingMode: "user",
+    }
+};
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    webcamVideo.srcObject = stream;
+}).catch((error) => {
+    console.error(error);
+});
 
 welcome.addEventListener("click", () => {
     // cancel refresh
