da4c8b2fff0ec12ae7b5c4272b255945bff03688
aki <kaiyunguyen@gmail.com>
2026-01-30 01:53:29 -0800

    Update effect.js

--- effect.js
+++ effect.js
@@ -3,10 +3,32 @@ let refreshTimer;
 // initial auto-refresh (15s)
 refreshTimer = setTimeout(() => {
     location.reload();
-}, 15000)
+}, 15000);
 
 const welcome = document.getElementById("welcomeScreen");
 const photobooth = document.getElementById("photoboothScreen");
+const countdownEl = document.getElementById("countdown");
+const shutterButton = document.getElementById("shutterButton");
+const video = document.getElementById("webcamVideo");
+const canvas = document.getElementById("snapshotCanvas");
+const ctx = canvas.getContext("2d");
+
+/* Webcam Video access */
+const webcamVideo = document.getElementById("webcamVideo");
+
+const constraints = {
+    video: {
+        height: 600,
+        width: 900,
+        facingMode: "user"
+    }
+};
+
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    webcamVideo.srcObject = stream;
+}).catch((error) => {
+    console.error(error);
+});
 
 welcome.addEventListener("click", () => {
     // cancel refresh
@@ -21,3 +43,63 @@ welcome.addEventListener("click", () => {
         location.reload();
     }, 300000);
 });
+
+shutterButton.addEventListener("click", () => {
+    startCountdown(() => {
+        flash();
+        const imageData = takeSnapshot();
+        console.log("Smile");
+        console.log(imageData);
+    })
+})
+
+function startCountdown(onComplete) {
+    let count = 5;
+    
+    countdownEl.style.color = "white";
+    countdownEl.textContent = count;
+    countdownEl.classList.remove("hidden");
+
+    const interval = setInterval(() => {
+        count--;
+
+        if (count === 1) {
+            countdownEl.style.color = "red";
+            setTimeout(() => {
+                countdownEl.classList.add("hidden");
+                if (onComplete) onComplete();
+            }, 700);
+            clearInterval(interval);
+        }
+
+        if (count == 0) {
+            clearInterval(interval);
+            countdownEl.classList.add("hidden");
+            if (onComplete) onComplete();
+            return;
+        }
+
+        countdownEl.textContent = count;
+    }, 1000);
+}
+
+function takeSnapshot() {
+    const videoHeight = video.videoHeight;
+    const videoWidth = video.videoWidth;
+
+    canvas.height = videoHeight;
+    canvas.width = videoWidth;
+
+    ctx.drawImage(video, 0, 0, videoWidth, videoHeight);
+}
+
+function flash() {
+    const flash = document.createElement("div");
+    flash.style.position = "absolute";
+    flash.style.inset = "0";
+    flash.style.background = "white";
+    flash.style.zIndex = "30";
+    document.body.appendChild(flash);
+
+    setTimeout(() => flash.remove(), 100);
+}
